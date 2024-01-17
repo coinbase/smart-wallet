44d30c938f4dbb045aa0c68117efe90c4a1f557c
Wilson Cusack <wilson.cusack@coinbase.com>
2024-01-16 20:11:36 -0500

    remove EIP712 for simplicity and directness

--- src/ERC1271.sol
+++ src/ERC1271.sol
@@ -3,14 +3,28 @@ pragma solidity ^0.8.4;
 
 import {EIP712} from "solady/src/utils/EIP712.sol";
 
-/// @notice ERC1271 mixin with nested EIP-712 approach, supporting multiple owners
-/// @author Solady (https://github.com/vectorized/solady/blob/main/src/accounts/ERC1271.sol)
+/// @notice ERC1271 with guards for same signer being used on multiple accounts
+/// Based on Solady (https://github.com/vectorized/solady/blob/main/src/accounts/ERC1271.sol)
 /// @author Wilson Cusack
-abstract contract ERC1271 is EIP712 {
-    bytes32 constant MESSAGE_TYPEHASH = keccak256("CoinbaseSmartAccountMessage(bytes32 message)");
+abstract contract ERC1271 {
+    bytes32 public immutable domainSeparator;
+    bytes32 private constant _MESSAGE_TYPEHASH = keccak256("CoinbaseSmartAccountMessage(bytes message)");
+
+    constructor() {
+        (string memory name, string memory version) = _domainNameAndVersion();
+        domainSeparator = keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes(name)),
+                keccak256(bytes(version)),
+                block.chainid,
+                address(this)
+            )
+        );
+    }
 
     function isValidSignature(bytes32 hash, bytes calldata signature) public view virtual returns (bytes4 result) {
-        if (_validateSignature(_hashTypedData(_messageHash(hash)), signature)) {
+        if (_validateSignature(replaySafeHash(abi.encode(hash)), signature)) {
             // bytes4(keccak256("isValidSignature(bytes32,bytes)"))
             return 0x1626ba7e;
         }
@@ -21,13 +35,47 @@ abstract contract ERC1271 is EIP712 {
     /// @dev Incase a signer is on multiple accounts, we expect all messages
     /// to be wrapped in an EIP 712 hash that includes the domain hash
     /// EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)
-    function replaySafeHash(bytes32 hash) public view returns (bytes32) {
-        return _hashTypedData(_messageHash(hash));
+    function replaySafeHash(bytes memory message) public view returns (bytes32) {
+        return _eip712Hash(_hashStruct(message));
     }
 
-    function _messageHash(bytes32 message) internal view virtual returns (bytes32) {
-        return keccak256(abi.encode(MESSAGE_TYPEHASH, message));
+    /// @dev See: https://eips.ethereum.org/EIPS/eip-5267
+    function eip712Domain()
+        public
+        view
+        virtual
+        returns (
+            bytes1 fields,
+            string memory name,
+            string memory version,
+            uint256 chainId,
+            address verifyingContract,
+            bytes32 salt,
+            uint256[] memory extensions
+        )
+    {
+        fields = hex"0f"; // `0b01111`.
+        (name, version) = _domainNameAndVersion();
+        chainId = block.chainid;
+        verifyingContract = address(this);
+        salt = salt; // `bytes32(0)`.
+        extensions = extensions; // `new uint256[](0)`.
     }
+
+    /// @dev encode(domainSeparator : 𝔹²⁵⁶, message : 𝕊) = "\x19\x01" ‖ domainSeparator ‖ hashStruct(message)
+    /// https://eips.ethereum.org/EIPS/eip-712
+    function _eip712Hash(bytes32 hashStruct) internal view returns (bytes32 digest) {
+        digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, hashStruct));
+    }
+
+    /// @dev hashStruct(s : 𝕊) = keccak256(typeHash ‖ encodeData(s))
+    /// https://eips.ethereum.org/EIPS/eip-712
+    function _hashStruct(bytes memory message) internal view virtual returns (bytes32) {
+        return keccak256(abi.encode(_MESSAGE_TYPEHASH, keccak256(message)));
+    }
+
+    /// @dev Please override this function to return the domain name and version.
+    function _domainNameAndVersion() internal view virtual returns (string memory name, string memory version);
 
     /// @dev Implement to vefify signature
     function _validateSignature(bytes32 message, bytes calldata signature) internal view virtual returns (bool);
