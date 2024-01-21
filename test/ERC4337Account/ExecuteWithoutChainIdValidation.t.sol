50b912acfd20fee4d89ca65f914572ec53bf498f
Wilson Cusack <wilson.cusack@coinbase.com>
2024-01-21 16:24:36 -0500

    revert changes for benchmark test

--- test/ERC4337Account/ExecuteWithoutChainIdValidation.t.sol
+++ test/ERC4337Account/ExecuteWithoutChainIdValidation.t.sol
@@ -52,7 +52,7 @@ contract TestExecuteWithoutChainIdValidation is AccountTestBase {
         UserOperation memory userOp = _getUserOpWithSignature();
         vm.expectEmit(true, true, true, true);
         emit UserOperationEvent(
-            entryPoint.getUserOpHash(userOp), userOp.sender, address(0), userOp.nonce, false, 0, 47573
+            entryPoint.getUserOpHash(userOp), userOp.sender, address(0), userOp.nonce, false, 0, 48245
         );
         _sendUserOperation(userOp);
     }
