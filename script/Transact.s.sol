0c5c41214a5e2843a0183c27921a30340e5e234d
Wilson Cusack <wilson.cusack@coinbase.com>
2024-01-23 09:25:36 -0500

    cleanup

--- script/Transact.s.sol
+++ script/Transact.s.sol
@@ -10,7 +10,7 @@ import "p256-verifier/src/P256.sol";
 
 contract TransactScript is Script {
     ERC4337Factory f = ERC4337Factory(0x748e5dD94d5F619371d763adfcBB0eDB863905fD);
-    uint256 passkeyPrivateKey = uint256(0x03d99692017473e2d631945a812607b23269d85721e0f370b8d3e7d29a874fd2);
+    uint256 passkeyPrivateKey = vm.envUint("P256_PRIVATE_KEY");
     bytes passkeyOwner =
         hex"1c05286fe694493eae33312f2d2e0d0abeda8db76238b7a204be1fb87f54ce4228fef61ef4ac300f631657635c28e59bfb2fe71bce1634c81c65642042f6dc4d";
     bytes[] owners;
@@ -19,9 +19,9 @@ contract TransactScript is Script {
     ERC4337Account account = ERC4337Account(payable(0xDCeE0dF9513eb53a55E9dE7bBc93A04F8DABd3F4));
 
     function run() public {
-      deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-      vm.startBroadcast(deployerPrivateKey);
-      console2.logBytes(account.ownerAtIndex(1));
+        deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        console2.logBytes(account.ownerAtIndex(1));
         // owners.push(abi.encode(vm.addr(deployerPrivateKey)));
         // owners.push(passkeyOwner);
         // account = ERC4337Account(payable(f.createAccount(owners, 1)));
@@ -43,19 +43,18 @@ contract TransactScript is Script {
         WebAuthnInfo memory webAuthn = Utils.getWebAuthnStruct(challenge);
         bytes32 check = webAuthn.messageHash;
         (bytes32 r, bytes32 s) = vm.signP256(passkeyPrivateKey, check);
-        ERC4337Account.PasskeySignature memory sig = 
-            ERC4337Account.PasskeySignature({
-                authenticatorData: webAuthn.authenticatorData,
-                clientDataJSON: webAuthn.clientDataJSON,
-                r: uint256(r),
-                s: uint256(s)
-            });
+        ERC4337Account.PasskeySignature memory sig = ERC4337Account.PasskeySignature({
+            authenticatorData: webAuthn.authenticatorData,
+            clientDataJSON: webAuthn.clientDataJSON,
+            r: uint256(r),
+            s: uint256(s)
+        });
 
         bytes memory sigWithOwnerIndex = abi.encodePacked(uint8(1), abi.encode(sig));
-        userOp.signature = sigWithOwnerIndex; 
+        userOp.signature = sigWithOwnerIndex;
         UserOperation[] memory ops = new UserOperation[](1);
         ops[0] = userOp;
-        
+
         entryPoint.handleOps{gas: 2_200_000}(ops, payable(vm.addr(deployerPrivateKey)));
     }
 }
