ce2dcc7a020c9579fef225732dc7610094d6befe
Lukas Rosario <36800180+lukasrosario@users.noreply.github.com>
2024-02-05 06:31:17 -0500

    comments

--- src/Universal1271InputGenerator.sol
+++ src/Universal1271InputGenerator.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-interface IERC1271Wallet {
-    function replaySafeHash(bytes32 hash) external view returns (bytes32);
-}
+import {ERC4337Account} from "./ERC4337Account.sol";
 
+/// @notice Helper contract to generate replay-safe hashes to sign for for ERC-1271 signature validation.
+/// Inspired by Ambire's DeploylessUniversalSigValidator (https://github.com/AmbireTech/signature-validator/blob/main/contracts/DeploylessUniversalSigValidator.sol)
+/// @author Lukas Rosario
 contract Universal1271InputGenerator {
-    constructor(address _account, bytes memory _encodedData) {
+    // This allows us to get a replay-safe hash on any deployed or undeployed account
+    // in a single eth_call. We do this by calling replaySafeHash on a deployed account,
+    // or by simularing the deployment of an undeployed account and then calling replaySafeHash on it.
+    constructor(ERC4337Account _account, bytes memory _encodedData) {
         bytes32 replaySafeHash = universal1271Input(_account, _encodedData);
         assembly {
             mstore(0x80, replaySafeHash)
@@ -14,26 +18,40 @@ contract Universal1271InputGenerator {
         }
     }
 
+    /// @dev Returns a replay-safe hash for a given account and encoded data. For an already
+    /// deployed account, encodedData should just be the hash we want a replay-safe version of.
+    /// For an undeployed account, encodedData should be wrapped to include the factory address,
+    /// the original hash, and the factory calldata.
+    /// @param account the account to get a replay-safe hash for.
+    /// @param encodedData the encoded data to use to get a replay-safe hash.
     function universal1271Input(
-        address account,
+        ERC4337Account account,
         bytes memory encodedData
     ) public returns (bytes32) {
         bytes memory contractCode = address(account).code;
 
+        // If the account is already deployed, call and return replaySafeHash.
         if (contractCode.length > 0) {
-            return IERC1271Wallet(account).replaySafeHash(bytes32(encodedData));
+            return account.replaySafeHash(bytes32(encodedData));
         }
 
+        // If the account is undeployed, deploy it and call replaySafeHash.
         address accountFactory;
         bytes32 originalHash;
         bytes memory factoryCalldata;
+        // Similar to ERC-6492 signatures, wrap the factory address, the
+        // original hash, and the factory calldata into encodedData
+        // so we can simulate deploying the account and then calling replaySafeHash.
         (accountFactory, originalHash, factoryCalldata) = abi.decode(
             encodedData,
             (address, bytes32, bytes)
         );
 
+        // Deploy the account.
         (bool success, ) = accountFactory.call(factoryCalldata);
         require(success, "Universal1271InputGenerator: deployment");
-        return IERC1271Wallet(account).replaySafeHash(originalHash);
+        
+        // Call and return replaySafeHash on the now-deployed account.
+        return account.replaySafeHash(originalHash);
     }
 }
