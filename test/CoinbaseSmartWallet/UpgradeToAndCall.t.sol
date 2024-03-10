f77b40a95e6b3d124f90813487c409f2b0141f70
Wilson Cusack <wilson.cusack@coinbase.com>
2024-03-10 19:55:38 -0400

    add UpgradeToAndCall test

---
+++ test/CoinbaseSmartWallet/UpgradeToAndCall.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {CoinbaseSmartWalletFactory} from "../../src/CoinbaseSmartWalletFactory.sol";
+
+import "./SmartWalletTestBase.sol";
+
+contract TestUpgradeToAndCall is SmartWalletTestBase {
+    address newImplementation = address(new Dummy());
+
+    function setUp() public override {
+        super.setUp();
+        CoinbaseSmartWalletFactory factory = new CoinbaseSmartWalletFactory(address(new CoinbaseSmartWallet()));
+        account = factory.createAccount(owners, 1);
+        vm.startPrank(signer);
+    }
+
+    function testUpgradeToAndCall() public {
+        account.upgradeToAndCall(newImplementation, abi.encodeWithSignature("dummy()"));
+    }
+}
+
+contract Dummy is UUPSUpgradeable {
+    event Done();
+
+    function dummy() public {
+        emit Done();
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
+}
