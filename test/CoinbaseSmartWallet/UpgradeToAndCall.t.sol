46278920941cdd02767b93aadf8d71e63704b930
katzman <steve.katzman@coinbase.com>
2024-03-18 15:10:58 -0700

    Add coverage for implementation in SmartContractWallet

--- test/CoinbaseSmartWallet/UpgradeToAndCall.t.sol
+++ test/CoinbaseSmartWallet/UpgradeToAndCall.t.sol
@@ -2,22 +2,29 @@
 pragma solidity ^0.8.0;
 
 import {CoinbaseSmartWalletFactory} from "../../src/CoinbaseSmartWalletFactory.sol";
+import {CoinbaseSmartWallet} from "../../src/CoinbaseSmartWallet.sol";
 
 import "./SmartWalletTestBase.sol";
 
 contract TestUpgradeToAndCall is SmartWalletTestBase {
-    address newImplementation = address(new Dummy());
+    address newImplementation = address(new CoinbaseSmartWallet());
 
     function setUp() public override {
         super.setUp();
         CoinbaseSmartWalletFactory factory = new CoinbaseSmartWalletFactory(address(new CoinbaseSmartWallet()));
         account = factory.createAccount(owners, 1);
-        vm.startPrank(signer);
     }
 
     function testUpgradeToAndCall() public {
+        vm.startPrank(signer);
         account.upgradeToAndCall(newImplementation, abi.encodeWithSignature("dummy()"));
         Dummy(address(account)).dummy();
+    }
+    
+    function testUpgradeToAndCallWithNonOwner() public {
+        vm.startPrank(address(1));
+        vm.expectRevert(MultiOwnable.Unauthorized.selector);  
+        account.upgradeToAndCall(newImplementation, abi.encodeWithSignature("dummy()"));
     }
 }
 
