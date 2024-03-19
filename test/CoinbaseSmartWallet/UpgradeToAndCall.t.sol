55060d756d1ede5cce2f13f91f4c0ad919fb1c7b
katzman <steve.katzman@coinbase.com>
2024-03-18 17:32:49 -0700

    remove superfluous test case

--- test/CoinbaseSmartWallet/UpgradeToAndCall.t.sol
+++ test/CoinbaseSmartWallet/UpgradeToAndCall.t.sol
@@ -2,29 +2,22 @@
 pragma solidity ^0.8.0;
 
 import {CoinbaseSmartWalletFactory} from "../../src/CoinbaseSmartWalletFactory.sol";
-import {CoinbaseSmartWallet} from "../../src/CoinbaseSmartWallet.sol";
 
 import "./SmartWalletTestBase.sol";
 
 contract TestUpgradeToAndCall is SmartWalletTestBase {
-    address newImplementation = address(new CoinbaseSmartWallet());
+    address newImplementation = address(new Dummy());
 
     function setUp() public override {
         super.setUp();
         CoinbaseSmartWalletFactory factory = new CoinbaseSmartWalletFactory(address(new CoinbaseSmartWallet()));
         account = factory.createAccount(owners, 1);
+        vm.startPrank(signer);
     }
 
     function testUpgradeToAndCall() public {
-        vm.startPrank(signer);
         account.upgradeToAndCall(newImplementation, abi.encodeWithSignature("dummy()"));
         Dummy(address(account)).dummy();
-    }
-
-    function testUpgradeToAndCallWithNonOwner() public {
-        vm.startPrank(address(1));
-        vm.expectRevert(MultiOwnable.Unauthorized.selector);
-        account.upgradeToAndCall(newImplementation, abi.encodeWithSignature("dummy()"));
     }
 }
 
