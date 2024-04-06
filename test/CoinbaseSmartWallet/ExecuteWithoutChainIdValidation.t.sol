50db60533af38874c7bf1802530f7bf2925ecabb
Wilson Cusack <wilson.cusack@coinbase.com>
2024-04-05 21:46:24 -0400

    more tests

--- test/CoinbaseSmartWallet/ExecuteWithoutChainIdValidation.t.sol
+++ test/CoinbaseSmartWallet/ExecuteWithoutChainIdValidation.t.sol
@@ -60,6 +60,19 @@ contract TestExecuteWithoutChainIdValidation is SmartWalletTestBase {
         account.executeWithoutChainIdValidation(calls);
     }
 
+    function test_reverts_whenOneCallReverts() public {
+        bytes4 selector = MultiOwnable.addOwnerAddress.selector;
+        assertTrue(account.canSkipChainIdValidation(selector));
+        address newOwner = address(6);
+        assertFalse(account.isOwnerAddress(newOwner));
+        calls.push(abi.encodeWithSelector(selector, newOwner));
+        calls.push(abi.encodeWithSelector(selector, newOwner));
+        userOpCalldata = abi.encodeWithSelector(CoinbaseSmartWallet.executeWithoutChainIdValidation.selector, calls);
+        vm.prank(address(entryPoint));
+        vm.expectRevert();
+        account.executeWithoutChainIdValidation(calls);
+    }
+
     function _sign(UserOperation memory userOp) internal view override returns (bytes memory signature) {
         bytes32 toSign = account.getUserOpHashWithoutChainId(userOp);
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPrivateKey, toSign);
