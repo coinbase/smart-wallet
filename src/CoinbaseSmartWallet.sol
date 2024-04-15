da9a09fa535851cab6eee978013021577fbd5183
Wilson Cusack <wilson.cusack@coinbase.com>
2024-04-15 09:35:11 -0400

    Make entryPoint an immutable variable

--- src/CoinbaseSmartWallet.sol
+++ src/CoinbaseSmartWallet.sol
@@ -42,6 +42,9 @@ contract CoinbaseSmartWallet is MultiOwnable, UUPSUpgradeable, Receiver, ERC1271
     /// @dev Helps enforce sequential sequencing of replayable transactions.
     uint256 public constant REPLAYABLE_NONCE_KEY = 8453;
 
+    /// @notice The address of the v0.6 EntryPoint contract.
+    address public entryPoint = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
+
     /// @notice Thrown when trying to re-initialize an account.
     error Initialized();
 
@@ -63,7 +66,7 @@ contract CoinbaseSmartWallet is MultiOwnable, UUPSUpgradeable, Receiver, ERC1271
 
     /// @notice Reverts if the caller is not the EntryPoint.
     modifier onlyEntryPoint() virtual {
-        if (msg.sender != entryPoint()) {
+        if (msg.sender != entryPoint) {
             revert Unauthorized();
         }
 
@@ -72,7 +75,7 @@ contract CoinbaseSmartWallet is MultiOwnable, UUPSUpgradeable, Receiver, ERC1271
 
     /// @notice Reverts if the caller is neither the EntryPoint, the owner, nor the account itself.
     modifier onlyEntryPointOrOwner() virtual {
-        if (msg.sender != entryPoint()) {
+        if (msg.sender != entryPoint) {
             _checkOwner();
         }
 
@@ -212,13 +215,6 @@ contract CoinbaseSmartWallet is MultiOwnable, UUPSUpgradeable, Receiver, ERC1271
         }
     }
 
-    /// @notice Returns the address of the EntryPoint v0.6.
-    ///
-    /// @return The address of the EntryPoint v0.6
-    function entryPoint() public view virtual returns (address) {
-        return 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
-    }
-
     /// @notice Computes the hash of the `UserOperation` in the same way as EntryPoint v0.6, but
     ///         leaves out the chain ID.
     ///
@@ -233,7 +229,7 @@ contract CoinbaseSmartWallet is MultiOwnable, UUPSUpgradeable, Receiver, ERC1271
         virtual
         returns (bytes32 userOpHash)
     {
-        return keccak256(abi.encode(UserOperationLib.hash(userOp), entryPoint()));
+        return keccak256(abi.encode(UserOperationLib.hash(userOp), entryPoint));
     }
 
     /// @notice Returns the implementation of the ERC1967 proxy.
