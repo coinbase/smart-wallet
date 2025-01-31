fac8066b8deb34e4dd39b9ad49e53b2397d540cc
Marson Kotovi4 <137284002+MarsonKotovi4@users.noreply.github.com>
2025-01-31 15:41:46 +0200

    Update MultiOwnable.sol

--- src/MultiOwnable.sol
+++ src/MultiOwnable.sol
@@ -35,7 +35,7 @@ contract MultiOwnable {
     ///      Computed from
     ///      keccak256(abi.encode(uint256(keccak256("coinbase.storage.MultiOwnable")) - 1)) & ~bytes32(uint256(0xff))
     ///      Follows ERC-7201 (see https://eips.ethereum.org/EIPS/eip-7201).
-    bytes32 private constant MUTLI_OWNABLE_STORAGE_LOCATION =
+    bytes32 private constant MULTI_OWNABLE_STORAGE_LOCATION =
         0x97e2c6aad4ce5d562ebfaa00db6b9e0fb66ea5d8162ed5b243f51a2e03086f00;
 
     /// @notice Thrown when the `msg.sender` is not an owner and is trying to call a privileged function.
